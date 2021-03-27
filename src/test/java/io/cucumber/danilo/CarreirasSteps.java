3c771ffda773b14888a3cce53a0b5e844b83233f
letiiciia <leticialimaferreira2@gmail.com>
2021-03-27 14:56:20 -0300

    caso de teste carreiras finalizado

--- src/test/java/io/cucumber/danilo/CarreirasSteps.java
+++ src/test/java/io/cucumber/danilo/CarreirasSteps.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import io.cucumber.danilo.servicos.Configuracoes;
+
 import io.cucumber.java.pt.*;
 
 
@@ -18,16 +19,17 @@ public class CarreirasSteps {
 	}
 
 	@Dado("clico no item do menu vagas em tecnologia")
-	public void clico_no_item_do_menu_vagas_em_tecnologia() {
+	public void clico_no_item_do_menu_vagas_em_tecnologia() throws InterruptedException {
+		Thread.sleep(2000);
 		WebElement input = Configuracoes.browser.findElement(By.cssSelector("#primaryLink4_Carreiras>div>div>ul>li:nth-child(3)>ul>li:nth-child(6)>a"));
-		input.click();
+		input.click();	
 	}
-
+	
 	@Entao("devo ver o destaque em {string}")
-	public void devo_ver_o_destaque_em(String string) {
+	public void devo_ver_o_destaque_em(String string) throws InterruptedException {
+		Thread.sleep(2000);
 		WebElement input = Configuracoes.browser.findElement(By.cssSelector("h1[class='area-of-interest-title page-title-long']") );
 		assertEquals(string, input.getText());
 		Configuracoes.fechar();
 	}
-
 }
--- src/test/resources/io/cucumber/danilo/carreiras.feature
+++ src/test/resources/io/cucumber/danilo/carreiras.feature
@@ -1,9 +1,11 @@
 #language: pt
 Funcionalidade: Acessar a lista de carreiras da accenture
   Criando uma rotina de teste para validar lista de carreiras
-	
+ 
   Cenario: Acessar o item de vagas de tecnologia
     Dado que eu estou no site da accenture "https://www.accenture.com/"
+    E clico no botao configuracao dos cookies
+    E aceito os termos LGPD
     E clico no menu carreiras
     E clico no item do menu vagas em tecnologia
-    Entao devo ver o destaque em "Carreiras em Tecnologia"
+    Entao devo ver o destaque em "Carreiras em tecnologia"
