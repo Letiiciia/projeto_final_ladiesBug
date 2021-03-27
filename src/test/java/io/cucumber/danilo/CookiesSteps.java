c6014a71885483b84584333e334a60dca4788bcf
letiiciia <leticialimaferreira2@gmail.com>
2021-03-27 10:27:08 -0300

    configurado a opcao clicar no botao

--- src/test/java/io/cucumber/danilo/CookiesSteps.java
+++ src/test/java/io/cucumber/danilo/CookiesSteps.java
@@ -1,11 +1,20 @@
 package io.cucumber.danilo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 import io.cucumber.danilo.servicos.Configuracoes;
 
 import io.cucumber.java.pt.*;
 
 
 public class CookiesSteps {
+	
+	@Dado("clico no botao configuracao dos cookies")
+	public void clico_no_botao_configuracao_dos_cookies() {
+		WebElement input = Configuracoes.browser.findElement(By.cssSelector(".cookie-setting-link"));
+		input.click();
+	}
 
 	@Dado("aceito os termos LGPD")
 	public void aceito_os_termos_LGPD() {
--- src/test/resources/io/cucumber/danilo/cookies.feature
+++ src/test/resources/io/cucumber/danilo/cookies.feature
@@ -4,6 +4,7 @@ Funcionalidade: Acessar o site da accenture e aceitar os cookies do LGPD
 
   Cenario: Aceitar o cookie LGPD
     Dado que eu estou no site da accenture "https://www.accenture.com/"
+    E clico no botao configuracao dos cookies
     E aceito os termos LGPD
     Entao deve fechar a caixa de informacao
     
